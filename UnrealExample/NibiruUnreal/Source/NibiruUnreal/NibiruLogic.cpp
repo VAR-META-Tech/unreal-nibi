9d5551e74bc69badc452eb2782833738c819343f
Dev BRX/ARX <nguyenducanhkhoa96@gmail.com>
2024-04-21 01:35:39 +0700

    Update Unreal UI
    Update Unreal UI

--- UnrealExample/NibiruUnreal/Source/NibiruUnreal/NibiruLogic.cpp
+++ UnrealExample/NibiruUnreal/Source/NibiruUnreal/NibiruLogic.cpp
@@ -55,21 +55,24 @@ void UNibiruLogic::OnCreateWalletClicked(FString &address_key_return, bool &IsCr
     auto convertedStr = StringCast<ANSICHAR>(*address);
     const char* queryAddress = convertedStr.Get();
     BaseAccount *account = QueryAccount((char*)queryAddress);
-    balance_return = "Balances :";
+    balance_return="";
+    //balance_return = "Balances :";
     if (account == NULL){
         error_return = "Failed to GetAccountBalance";
         return;
     }
-    for (int i=0; i < account->Coins->Length; i++){
-        balance_return += account->Coins->Array[0].Denom;
-        balance_return += " - ";
-        balance_return += std::to_string(account->Coins->Array[0].Amount).c_str();
-        balance_return += ";";
-    }
+    balance_return += std::to_string(account->Coins->Array[0].Amount).c_str();
+    // for (int i=0; i < account->Coins->Length; i++){
+    //     balance_return += account->Coins->Array[0].Denom;
+    //     balance_return += " - ";
+    //     balance_return += std::to_string(account->Coins->Array[0].Amount).c_str();
+    //     balance_return += ";";
+    // }
     IsSuccess = true;
  }
 
 void UNibiruLogic::OnFaucetClicked(FString address_received, bool &IsSuccess, FString &error_return){
+    error_return ="";
     IsSuccess = false;
     char *adminMnemonic = strdup("guard cream sadness conduct invite crumble clock pudding hole grit liar hotel maid produce squeeze return argue turtle know drive eight casino maze host");
     char *passPhase = strdup("pass");
@@ -101,12 +104,14 @@ void UNibiruLogic::OnFaucetClicked(FString address_received, bool &IsSuccess, FS
 
 void UNibiruLogic::OnTransferClicked(FString from_address, FString to_address, FString demon, int amount, bool &IsSuccess, FString &error_return){
     IsSuccess = false;
+    error_return = "";
     auto convertedStr = StringCast<ANSICHAR>(*from_address);
     const char* fromAddress = convertedStr.Get();
     auto convertedStr2 = StringCast<ANSICHAR>(*to_address);
     const char* toAddress = convertedStr2.Get();
     auto convertedStr3 = StringCast<ANSICHAR>(*demon);
     const char* demonStr = convertedStr3.Get();
+    
     int tx = TransferToken((char*)fromAddress, (char*)toAddress, (char*)demonStr, amount);
     delete[] fromAddress;
     delete[] toAddress;
